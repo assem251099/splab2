31b07327213f2469562b46d83ae387da8d592c5f
assem251099 <57049483+assem251099@users.noreply.github.com>
2019-11-11 22:36:24 +0600

    Create Subtraction.java

---
+++ MATHDO/Subtraction.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Subtraction{
+	public static void main(String[] args) {
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		System.out.println("java: "+a+"-"+b+"="+(a-b));
+	}
+}
