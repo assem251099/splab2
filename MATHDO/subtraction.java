f7672ca72c0fa09af01de2ed84ae2e0fef882a5f
assem251099 <57049483+assem251099@users.noreply.github.com>
2019-11-11 22:47:33 +0600

    Update and rename Subtraction.class to subtraction.java

--- MATHDO/subtraction.java
+++ MATHDO/subtraction.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Subtraction{
+public class subtraction{
 	public static void main(String[] args) {
 		int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[1]);
