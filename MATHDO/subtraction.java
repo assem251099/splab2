326745258598996d1616eb36d27e25e4c040d669
assem251099 <57049483+assem251099@users.noreply.github.com>
2019-11-11 23:51:21 +0600

    Delete subtraction.java

--- MATHDO/subtraction.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class subtraction{
-	public static void main(String[] args) {
-		int a = Integer.parseInt(args[0]);
-		int b = Integer.parseInt(args[1]);
-		System.out.println("java: "+a+"-"+b+"="+(a-b));
-	}
-}
